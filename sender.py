5b849f0dc29a6b1f2b8342b2fe2ccb4e588a2036
Jarred Luzada <jarredluzada@gmail.com>
2022-05-25 18:51:17 +0800

    Added arg flags.

--- sender.py
+++ sender.py
@@ -1,27 +1,46 @@
 import requests
 import socket
+import argparse
 
 PID = "95b59f86"
+SENDER_PORT = 6716
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-f", "--file", help="File to send", default="payload.txt")
+parser.add_argument("-a", "--address",
+                    help="Server IP address", default="10.0.7.141")
+parser.add_argument("-s", "--receiver_port", type=int,
+                    help="Port number used by the receiver", default=9000)
+parser.add_argument("-c", "--sender_port", type=int,
+                    help="Port number used by the sender", default=SENDER_PORT)
+parser.add_argument("-i", "--id", type=str,
+                    help="Unique Identifier", default=PID)
+
+args = parser.parse_args()
+
+print(args)
 
 # Downloading the payload
 
 URL = f"http://3.0.248.41:5000/get_data?student_id={PID}"
 response = requests.get(URL)
-open("payload.txt", "wb").write(response.content)
+open(args.file, "wb").write(response.content)
 
 # Establishing a UDP connection with the server
-UDP_SENDER_PORT_NO = 6716
-UDP_RECEIVER_PORT_NO = 9000
-UDP_IP_ADDRESS = "54.169.205.232"
+UDP_SENDER_PORT_NO = args.sender_port
+UDP_RECEIVER_PORT_NO = args.receiver_port
+UDP_IP_ADDRESS = args.address
+
+clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+serverSock.bind(('', UDP_RECEIVER_PORT_NO))
 
 # Sending an intent message
 
 imessage = f"ID{PID}".encode()
-
-clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 clientSock.sendto(imessage, (UDP_IP_ADDRESS, UDP_SENDER_PORT_NO))
 
-serverSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverSock.bind(('', UDP_RECEIVER_PORT_NO))
-
 TID = serverSock.recvfrom(UDP_RECEIVER_PORT_NO)
+print(TID)
