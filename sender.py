8b1e0b09ed61613c1668efb9c5e5422c636d1708
Jarred Luzada <jarredluzada@gmail.com>
2022-05-27 03:03:14 +0800

    Fixed receiver.

--- sender.py
+++ sender.py
@@ -97,7 +97,7 @@ class Sender:
 
             if self.verifyAck(seqID, ack, packet, duration):
                 sent += size
-                size = int(len(data) // (120 / rate))
+                size = int(len(data) // (90 / rate))
                 seq += 1
 
     def verifyAck(self, seqID, ack, packet, duration):
