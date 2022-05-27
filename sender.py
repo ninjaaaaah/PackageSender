1e2e0f6c58db3ab243f92ec2f69d1f66b2380e81
Jarred Luzada <jarredluzada@gmail.com>
2022-05-27 22:40:16 +0800

    Log

--- sender.py
+++ sender.py
@@ -129,7 +129,7 @@ class Sender:
 
                 duration = t1 - t0
 
-                rate = (rate + duration) / 2 if rate != 0 else rate
+                rate = (rate + duration) / 2 if rate != 0 else duration
 
                 ack = reply.decode()
 
