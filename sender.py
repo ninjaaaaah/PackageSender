9010730145182ad2d441a48ca687420c34165641
Jarred Luzada <jarredluzada@gmail.com>
2022-05-28 01:27:35 +0800

    Batch

--- sender.py
+++ sender.py
@@ -168,7 +168,7 @@ class Sender:
     def waitEnd(self):
         print()
         while True:
-            remaining = 120 - (time.time() - self.timer)
+            remaining = 125 - (time.time() - self.timer)
             print("\033[A                             \033[A")
             print(
                 f"{120 - (time.time() - self.timer):.2f}s | [{('█'*int(math.ceil(remaining/120 *10))).ljust(10)}]")
