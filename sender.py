221194b097da25a1ef1c6e71623395f2cab47ff1
Jarred Luzada <jarredluzada@gmail.com>
2022-05-27 23:55:18 +0800

    Log

--- sender.py
+++ sender.py
@@ -101,7 +101,7 @@ class Sender:
             t0 = time.time()
 
             if rate != 0:
-                self.sock.settimeout(rate+1)
+                self.sock.settimeout(rate+1.5)
 
             try:
                 reply, _ = self.sock.recvfrom(self.RECEIVER_PORT_NO)
