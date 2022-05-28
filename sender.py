deb06a1f44346efec7fa9459167417b09a41db73
Jarred Luzada <jarredluzada@gmail.com>
2022-05-28 17:47:38 +0800

    try except

--- sender.py
+++ sender.py
@@ -124,9 +124,10 @@ class Sender:
                 target = target if elapsed < target else 120
                 rem_time = target - elapsed
                 rem_data = (len(data)-sent)
-                size = max(math.ceil(rem_data / rem_time * (rate+1)), last)
-                size = size if size < limit else min(math.floor(
-                    (seq*size+limit) / (seq+1)), limit-1)
+                if eta > 95:
+                    size = max(math.ceil(rem_data / rem_time * (rate+1)), last)
+                    size = size if size < limit else min(math.floor(
+                        (seq*last+limit) / (seq+1)), limit-1)
                 seq += 1
 
             except socket.timeout:
